c5d035909b3bdc0a662eed5966da3fa54309ecbc
nlko <nospam1@thomasson.fr>
2014-01-23 17:12:08 +0100

    Add support for load, render, ...
    Support for load, score, onInitialize, onChange, onItemAdd,
    onItemRemove, onClear, onDelete, onOptionAdd, onOptionRemove,
    onDropdownOpen, onDropdownClose, onType, onLoad, render. (see selective
    documentation)

--- README.md
+++ README.md
@@ -9,5 +9,9 @@ Directive: 'selectize'
 Using:
 
 ``
-  <select name="" selectize="{  delimiter: ',', persist: false }">
+  <select name="" selectize="{  delimiter: ',', persist: false, load:'my_load' }">
 ``
+
+
+ - load : name of a function in the scope to call on the load event (see selective documentation).
+ - The same appy for the following function: score, onInitialize, onChange, onItemAdd, onItemRemove, onClear, onDelete, onOptionAdd, onOptionRemove, onDropdownOpen, onDropdownClose, onType, onLoad, render
--- dist/ng-selectize.js
+++ dist/ng-selectize.js
@@ -6,7 +6,24 @@
       restrict: "AE",
       link: function(scope, element, attrs) {
         return $timeout(function() {
-          return $(element).selectize(scope.$eval(attrs.selectize));
+            var options = scope.$eval(attrs.selectize); 
+
+			// list of selective functions
+            var option_func=['load','score','onInitialize','onChange','onItemAdd','onItemRemove','onClear','onDelete','onOptionAdd','onOptionRemove',
+            'onDropdownOpen','onDropdownClose','onType','onLoad','render'];
+
+			// convert every function name to a call to the function in the scope
+			// For example, if we have in the controler
+			// scope.my_load = function(query,cb) {}
+			// and the html option contains 
+			// <select ... selectize="{ load:'my_load', delimiter: ',', ...}">
+			// this will make load option point to the my_load function from the scope
+            option_func.forEach(function(elem,index,array){
+              if(options[elem])
+                options[elem] = scope[options[elem]];
+            });
+
+          return $(element).selectize(options);
         });
       }
     };
