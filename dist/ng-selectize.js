79eec80863342fb5cfaee365bbc6ff959772a2aa
nlko <nospam1@thomasson.fr>
2014-01-30 15:16:51 +0100

    Correct for render options support

--- dist/ng-selectize.js
+++ dist/ng-selectize.js
@@ -6,22 +6,32 @@
       restrict: "AE",
       link: function(scope, element, attrs) {
         return $timeout(function() {
-            var options = scope.$eval(attrs.selectize); 
+            var options = scope.$eval(attrs.selectize);
 
 			// list of selective functions
             var option_func=['load','score','onInitialize','onChange','onItemAdd','onItemRemove','onClear','onDelete','onOptionAdd','onOptionRemove',
-            'onDropdownOpen','onDropdownClose','onType','onLoad','render'];
+            'onDropdownOpen','onDropdownClose','onType','onLoad'];
 
 			// convert every function name to a call to the function in the scope
 			// For example, if we have in the controler
 			// scope.my_load = function(query,cb) {}
-			// and the html option contains 
+			// and the html option contains
 			// <select ... selectize="{ load:'my_load', delimiter: ',', ...}">
 			// this will make load option point to the my_load function from the scope
             option_func.forEach(function(elem,index,array){
               if(options[elem])
                 options[elem] = scope[options[elem]];
             });
+
+            if(options['render']) {
+              var render_func=['option','option_create','optgroup_header','optgroup'];
+
+              render_func.forEach(function(elem,index,array){
+                options['render'][elem] = scope[options['render'][elem]];
+                });
+            }
+
+            console.dir(attrs);
 
           return $(element).selectize(options);
         });
